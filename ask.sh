766f3173aa55b591d0cd5d69ceb2874ef97b2d56
pedrosobucki <pedrosobucki@gmail.com>
2024-01-29 21:20:43 -0300

    saves history in .json file

--- ask.sh
+++ ask.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OUTPUT=/dev/stdout
+
 if [ -z $KEY ]; then
 	echo "You must provide an API KEY."
 	exit 1
@@ -16,18 +18,43 @@ help_info() {
 	exit 0
 }
 
+save_to_hist() {
+    LOG=$(cat $HIST_DIR/chat1.json | jq ".hist += [$1]")
+    echo -E $LOG > $HIST_DIR/chat1.json
+}
+
+RAW=false
+
 # read optional flags
-while getopts ":t:T:m:h" opt; do
+while getopts ":t:T:m:rh" opt; do
     case $opt in
         h) help_info;;
         t) MAX_TOKENS=$OPTARG;;
         T) TEMPERATURE=$OPTARG;;
         m) MODEL=$OPTARG;;
+        r) RAW=true;;
         ?) echo "Invalid option \"-$OPTARG\""; exit 1;;
     esac
 done
 
-echo -ne "  [ Generating... ]\033[0K\r"
+RAW_MOD=
+if [ $RAW = "true" ]; then
+    RAW_MOD=". This specific response should only be code, without explanations, markdown formating or anything other than the code itself."
+    OUTPUT=/dev/null
+fi
+
+CURRENT_DIR=
+if [ $KNOW_CURRENT_DIR = "true" ]; then
+	CURRENT_DIR="You are currently in the $PWD directory."
+fi
+
+# treats prompt special chars for json format
+PROMPT='{
+	"role": "user",
+	"content": "'$(echo -E ${@: -1} $STDIN | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g')$RAW_MOD'"
+}'
+
+echo -ne "  [ Generating... ]\033[0K\r" > $OUTPUT
 
 BODY='{
 	"model": "'$MODEL'",
@@ -56,21 +83,31 @@ if [ "$ERROR" != "null" ]; then
 fi
 
 MESSAGE=$(echo -E $RESPONSE | jq '.choices | .[] | .message')
-echo -E cat $HIST_DIR/chat1.json | jq ".hist += [${MESSAGE}]"
-exit
 
 # saves prompt
-cat $HIST_DIR/chat1.json | jq ".hist += [${PROMPT}]" > $HIST_DIR/chat1.json
+LOG=$(cat $HIST_DIR/chat1.json | jq ".hist += [${PROMPT}]")
+echo -E $LOG > $HIST_DIR/chat1.json
 
-ANSWER=$(echo -E $RESPONSE | jq '.choices | .[] | .message.content')
+ANSWER=$(echo -E $MESSAGE | jq '.content')
 
 # saves context
-cat $HIST_DIR/chat1.json | jq ".hist += [$(echo -E $RESPONSE | jq '.choices | .[] | .message')]" > $HIST_DIR/chat1.json
+LOG=$(cat $HIST_DIR/chat1.json | jq ".hist += [$MESSAGE]")
+echo -E $LOG > $HIST_DIR/chat1.json
 
 # erases old chat chat history
-if [ $(cat $HIST_DIR/chat1.json | jq '.hist | length') -gt "$((MAX_CHAT_MEMORY * 2 + 1))" ]; then
-	# awk 'NR>2' $HIST_DIR/chat1.json > $SCRIPT_PATH/tmp && mv $SCRIPT_PATH/tmp $HIST_DIR/chat1.json
-    cat $HIST_DIR/chat1.json | jq '.hist | del(.[-2:])' > $HIST_DIR/chat1.json
+if [ "$(cat $HIST_DIR/chat1.json | jq '.hist | length')" == "$((MAX_CHAT_MEMORY * 2 + 1))" ]; then
+    LOG=$(cat $HIST_DIR/chat1.json | jq '.hist | del(.[-2:])')
+    echo -E $LOG > $HIST_DIR/chat1.json
 fi
 
-echo -e $ANSWER
+# parsed json response to regular string
+PARSED=$(echo -E $ANSWER | sed 's/\\\"/\"/g')
+PARSED=${PARSED:1:-1}
+
+# removes markdown code block if exists and wants raw output
+if [ $RAW = "true" ]; then
+    echo -e $PARSED | sed '/```/d'
+    exit 0
+fi
+
+echo -e $PARSED
